0988a80efb2a11bbc2fbb5be7c871e754df501c8
Nyemot3rd <111175318+Nyemot3rd@users.noreply.github.com>
2023-03-24 07:38:39 +0800

    Update 13055.cpp

--- 2023-03-21/13055.cpp
+++ 2023-03-21/13055.cpp
@@ -1,22 +1,24 @@
 #include<iostream>
-#include<vector>
+#include<stack>
 using namespace std;
 
 int main(){
 	int n;
-	vector<string> name;
+	stack<string> name;
 	string s;
 	cin>>n;
 	while(n--){
 		cin>>s;
-		if(s[0]=='S'&&cin>>s){
-			name.push_back(s);
+		if(s[0]=='S'){
+			cin>>s;
+			name.push(s);
 		}else if(s[0]=='K'){
-			if(!name.empty())name.pop_back();
+			if(!name.empty())
+				name.pop();
 		}else if(name.empty()){
 			cout<<"Not in a dream\n";
 		}else{
-			cout<<name.back()<<'\n';
+			cout<<name.top()<<'\n';
 		}
 	}
 }
